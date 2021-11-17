d67397330019561ed62050013650a912ff457b13
todmeyek <63090107+todmeyek@users.noreply.github.com>
2021-11-17 10:03:54 +0200

    TestVSCode

--- SoloTest.py
+++ SoloTest.py
@@ -1,8 +1,23 @@
-N = int(input("Enter number for sum: "))
-sum = 0
+# Sum of all numbers
+# N = int(input("Enter number for sum: "))
+# sum = 0
 
-for n in range (0, N+1, 1):
-    sum = sum + n
-print("Sum of all numbers is: ", sum)
+# for n in range (0, N+1, 1):
+#     sum = sum + n
+# print("Sum of all numbers is: ", sum)
 
 
+# Search Word in text
+
+text = input("Enter Text: ")
+word = input("Enter keyword: ")
+
+
+def search(text, word):
+    if word in text:
+        print("Word found")
+    else:
+        print("Word not found")
+
+
+search(text, word)
