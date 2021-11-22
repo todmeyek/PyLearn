f80c5093c6abfa776f87c229535dd57aa344d502
todmeyek <63090107+todmeyek@users.noreply.github.com>
2021-11-22 15:49:24 +0200

    h

--- SoloTest.py
+++ SoloTest.py
@@ -6,17 +6,33 @@
 #     sum = sum + n
 # print("Sum of all numbers is: ", sum)
 
+########################################################
 
 
 # Search Word in text
-text = input("Enter Text: ")
-word = input("Enter keyword: ")
+# text = input("Enter Text: ")
+# word = input("Enter keyword: ")
 
-def search(text, word):
-    if word in text:
-        print("Word found")
+# def search(text, word):
+#     if word in text:
+#         print("Word found")
+#     else:
+#         print("Word not found")
+
+# search(text, word)
+########################################################
+
+#FizzBuzz
+n = int(input("Enter the number for FizzBuzz: "))
+for x in range(1, n):
+    n = -1
+    if x % 3 == 0 and x % 5 == 0:
+        print("SoloLearn")
+    elif x % 3 == 0:
+        print("Solo")
+    elif x % 5 == 0:
+        print("Learn")
     else:
-        print("Word not found")
-
-search(text, word)
-###
+        print(x)
+        continue
+    
