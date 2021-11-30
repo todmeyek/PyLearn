dc3a7399ce495b44b28bf8b652ed9c02a85ed113
todmeyek <63090107+todmeyek@users.noreply.github.com>
2021-11-30 23:20:11 +0200

    Lr

--- w3resource/Basic1/5.IreverseName.py
+++ w3resource/Basic1/5.IreverseName.py
@@ -1,4 +1,29 @@
-name = input("Your name?")
-vorname = input("your vorname?")
+# name = input("Your name?")
+# vorname = input("your vorname?")
 
-print( "Hello Mr. " + vorname + " " + name)
+# print( "Hello Mr. " + vorname + " " + name)
+
+
+# 2 Method
+# namevorname = input("Your Name and vorname: ")
+# namevornamelist = namevorname.split(" ")
+# revlist = reversed(namevornamelist)
+# print(list(revlist))
+
+
+# 3 Method
+# x = input("Your Name and vorname: ")
+# y = x.split(" ")
+
+# for item in reversed(y):
+#     print (item)
+
+# 4 Method
+# x = input("Your Name and vorname: ")
+# y = x.split(' ')
+# y.reverse()
+# print(" ".join(y))
+
+# 5 Method
+x = input("Your Name and vorname: ")
+print(" ".join(x.split(' ')[::-1]))
