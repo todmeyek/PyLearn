9691827f4c96bf9c23dc2105f7e7efc085eb5c18
todmeyek <63090107+todmeyek@users.noreply.github.com>
2021-12-13 00:03:32 +0200

    a

--- SoloTest.py
+++ SoloTest.py
@@ -1,24 +1,20 @@
 # Sum of all numbers
 # N = int(input("Enter number for sum: "))
 # sum = 0
-
 # for n in range (0, N+1, 1):
 #     sum = sum + n
 # print("Sum of all numbers is: ", sum)
-
 ########################################################
 
 
 # Search Word in text
 # text = input("Enter Text: ")
 # word = input("Enter keyword: ")
-
 # def search(text, word):
 #     if word in text:
 #         print("Word found")
 #     else:
 #         print("Word not found")
-
 # search(text, word)
 ########################################################
 
@@ -36,8 +32,7 @@
 #     else:
 #         print(x)
 #         continue
-
-#     #### Test23
+########################################################
 
 # Read content from file and then return book codes(First char of title + lenght)
 file = open("D:\Mokslai\GitHub\PyLearn\Movies.txt", "r")
