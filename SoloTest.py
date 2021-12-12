cd213c70c0d5dca5ee4994d52e23ffe967c2026a
todmeyek <63090107+todmeyek@users.noreply.github.com>
2021-12-13 00:02:15 +0200

    f

--- SoloTest.py
+++ SoloTest.py
@@ -22,20 +22,28 @@
 # search(text, word)
 ########################################################
 
-#FizzBuzz
-n = int(input("Enter the number for FizzBuzz: "))
-for x in range(1, n):
-    if x % 2 == 0:
-        continue
-    elif x % 3 == 0 and x % 5 == 0:
-        print("SoloLearn")
-    elif x % 3 == 0:
-        print("Solo")
-    elif x % 5 == 0:
-        print("Learn")
-    else:
-        print(x)
-        continue
+# #FizzBuzz
+# n = int(input("Enter the number for FizzBuzz: "))
+# for x in range(1, n):
+#     if x % 2 == 0:
+#         continue
+#     elif x % 3 == 0 and x % 5 == 0:
+#         print("SoloLearn")
+#     elif x % 3 == 0:
+#         print("Solo")
+#     elif x % 5 == 0:
+#         print("Learn")
+#     else:
+#         print(x)
+#         continue
 
-    #### Test23
-    
+#     #### Test23
+
+# Read content from file and then return book codes(First char of title + lenght)
+file = open("D:\Mokslai\GitHub\PyLearn\Movies.txt", "r")
+# cont = file.read
+for line in file:
+    line = line.strip("\n")
+    ch = len(line)
+    print(line[0] + str(ch))
+file.close()
