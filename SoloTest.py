b8c83477869ea2b1d3996f54d0551fab6ffeb09b
Kestutis Mitkus <Kestutis.Mitkus@bentley.com>
2022-01-12 20:55:23 +0200

    j

--- SoloTest.py
+++ SoloTest.py
@@ -43,8 +43,12 @@
 #     print(line[0] + str(ch))
 # file.close()
 
-N = int(input("Enter number for sum: "))
-sum = 0
-for n in range (0, N+1, N-1):
-    sum = sum + n
-print("Sum of all numbers is: ", sum)
+num = int(input("Enter number for fib: "))
+
+def fib(n):
+    for n in range (0, n+1, 1):
+        sum = n
+        
+        print(sum)
+
+fib(num)
